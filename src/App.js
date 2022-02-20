3ded3b3298da17f17524b0022308cb14534a2f24
Salim A. JOLY <el.salim.salim@gmail.com>
2022-02-20 20:09:12 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 const URL = "http://127.0.0.1:3000/adventures/test.json";
 
 function App() {
-  const [adventure, setAdventure] = useState();
+  const [adventure, setAdventure] = useState(null);
 
   const readJson = (url) => {
     axios.get(url)
@@ -14,6 +14,26 @@ function App() {
         const data = res.data;
         setAdventure(data);
       })
+      .catch((error) => {
+        // Error
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the 
+            // browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+    });
   }
 
 
@@ -23,23 +43,24 @@ function App() {
   }, []);
 
 
-  return (
-    <div className="App">
-      
-      <header>
-        <h1 className="Home-title">{adventure.info.name}</h1>
-      </header>
-      
-      <nav>Nav...</nav>
-      
-      <main>
-        <Home title={adventure.info.name} description={adventure.info.description} image={adventure.info.description} />
-      </main>
-      
-      <footer>
-        Author: {adventure.info.author.fullname}
-      </footer>
-    </div>
+  return (adventure == null)
+    ? <h1>ERROR : Object "adventure" not initialized.</h1>
+    : (
+      <div className="App">        
+        <header>
+          <h1 className="Home-title">{adventure.info.name}</h1>
+        </header>
+        
+        <nav>Nav...</nav>
+        
+        <main>
+          <Home title={adventure.info.name} description={adventure.info.description} image={adventure.info.description} />
+        </main>
+        
+        <footer>
+          Author: {adventure.info.author.fullname}
+        </footer>
+      </div>
   );
 }
 
